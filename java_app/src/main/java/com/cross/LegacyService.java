d581a387ff9bd1bdf792488a08122dd9f22ae75e
Tung-Xie <tony.xie@chelpis.com>
2026-02-26 11:45:27 +0800

    Add files via upload

--- java_app/src/main/java/com/cross/LegacyService.java
+++ java_app/src/main/java/com/cross/LegacyService.java
@@ -1,9 +1,23 @@
 package com.cross;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import java.security.*;
+
 public class LegacyService {
-    public void run() throws Exception {
+    public void runLegacy() throws Exception {
+        // [High Risk] RSA-1024
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
         kpg.initialize(1024);
-        MessageDigest.getInstance("MD5").digest();
+        
+        // [High Risk] MD5
+        MessageDigest.getInstance("MD5").digest("data".getBytes());
+
+        // [Medium Risk] AES-128-CBC
+        byte[] keyBytes = new byte[16]; // 128 bits
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(new byte[16]));
     }
 }
--- python_app/crypto_logic.py
+++ python_app/crypto_logic.py
@@ -1,22 +1,19 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
 
-def trigger_high_risk_python():
-    # --- RSA-1024 (High Risk) ---
-    # 使用 cryptography 庫的標準生成方式，CBOMkit 對此特徵最敏感
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=1024, # 這裡標記 1024
-    )
+def cross_lang_risk():
+    # [High Risk] RSA-1024
+    priv_key = rsa.generate_private_key(public_exponent=65537, key_size=1024)
 
-    # --- MD5 (High Risk) ---
-    # 明確呼叫 hashes.MD5()
+    # [High Risk] MD5
     digest = hashes.Hash(hashes.MD5())
-    digest.update(b"bad_idea")
-    result = digest.finalize()
+    digest.update(b"shared_secret")
+    digest.finalize()
 
-    # --- SHA-1 (High Risk) ---
-    digest_sha1 = hashes.Hash(hashes.SHA1())
-    digest_sha1.finalize()
-
-    return "Python Crypto Triggered"
+    # [Medium Risk] AES-128-CBC (跨語言常用，但 CBC 需要 IV 管理)
+    key = os.urandom(16) # 128 bits
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
