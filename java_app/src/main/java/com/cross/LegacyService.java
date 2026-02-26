cfd0d5c1dd417b8d7ff112260f4dfd0c1a8deaa1
Tung-Xie <tony.xie@chelpis.com>
2026-02-26 11:42:26 +0800

    Add files via upload

--- java_app/src/main/java/com/cross/LegacyService.java
+++ java_app/src/main/java/com/cross/LegacyService.java
@@ -1,11 +1,9 @@
 package com.cross;
 import java.security.*;
 public class LegacyService {
-    public void init() throws Exception {
-        // 同樣的 RSA-1024
+    public void run() throws Exception {
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
         kpg.initialize(1024);
-        // 同樣的 MD5
         MessageDigest.getInstance("MD5").digest();
     }
 }
--- python_app/crypto_logic.py
+++ python_app/crypto_logic.py
@@ -1,11 +1,22 @@
-from Crypto.PublicKey import RSA
-from Crypto.Hash import MD5
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
 
-def legacy_crypto():
-    # 這裡也用了 RSA-1024 (High Risk)
-    key = RSA.generate(1024)
-    
-    # 這裡也用了 MD5
-    h = MD5.new()
-    h.update(b'data')
-    return h.hexdigest()
+def trigger_high_risk_python():
+    # --- RSA-1024 (High Risk) ---
+    # 使用 cryptography 庫的標準生成方式，CBOMkit 對此特徵最敏感
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=1024, # 這裡標記 1024
+    )
+
+    # --- MD5 (High Risk) ---
+    # 明確呼叫 hashes.MD5()
+    digest = hashes.Hash(hashes.MD5())
+    digest.update(b"bad_idea")
+    result = digest.finalize()
+
+    # --- SHA-1 (High Risk) ---
+    digest_sha1 = hashes.Hash(hashes.SHA1())
+    digest_sha1.finalize()
+
+    return "Python Crypto Triggered"
